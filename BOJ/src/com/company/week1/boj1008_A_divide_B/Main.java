174a2da3c70e5249b156f0bccdfb57626bae9760
hwlee0103 <sdiverHyeWon0103@gmail.com>
2022-06-05 08:38:37 +0900

    BOJ 1008 A/B

---
+++ BOJ/src/com/company/week1/boj1008_A_divide_B/Main.java
@@ -0,0 +1,19 @@
+package com.company.week1.boj1008_A_divide_B;
+
+import java.util.Scanner;
+
+/**
+ * BOJ 1008 A/B
+ */
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        Scanner input = new Scanner(System.in);
+
+        int a = input.nextInt();
+        int b = input.nextInt();
+
+        System.out.println((double)a/b);
+    }
+}
