6f81ca039217607fe61f0a4e9e014a842eab1742
hwlee0103 <sdiverHyeWon0103@gmail.com>
2022-06-05 08:34:21 +0900

    BOJ 1000, 1001, 25083, 10998

---
+++ BOJ/src/com/company/week1/boj1000_A_plus_B/Main.java
@@ -0,0 +1,19 @@
+package com.company.week1.boj1000;
+
+import java.util.Scanner;
+
+/**
+ * BOJ 1000 A+B
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+        Scanner input = new Scanner(System.in);
+
+        int a = input.nextInt();
+        int b = input.nextInt();
+
+        System.out.println(a + b);
+    }
+}
