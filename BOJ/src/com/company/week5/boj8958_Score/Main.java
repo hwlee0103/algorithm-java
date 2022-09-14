5dfacc27f151a51a0398dacde831c3053355d5bf
hwlee0103 <sdiverHyeWon0103@gmail.com>
2022-09-14 16:30:10 +0900

    BOJ 8958 Score 풀이 진행중

---
+++ BOJ/src/com/company/week5/boj8958_Score/Main.java
@@ -0,0 +1,23 @@
+package com.company.week5.boj8958_Score;
+
+import java.util.Scanner;
+
+/**
+ * BOJ 8958 Score
+ */
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int t = input.nextInt();
+
+        for(int i = 0; i < t ; ++i) {
+            String test = "";
+            test = input.next();
+            int[] score = new int[test.length()];
+
+            for(int j = 0; j < test.length(); ++j) {
+                if(test[j].val == "O")
+            }
+        }
+    }
+}
