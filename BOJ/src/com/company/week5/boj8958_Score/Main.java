84233ae6115a87ba94004e1140665779fea01cf3
hwlee0103 <sdiverHyeWon0103@gmail.com>
2022-09-20 22:27:57 +0900

    BOJ 8958 Score

--- BOJ/src/com/company/week5/boj8958_Score/Main.java
+++ BOJ/src/com/company/week5/boj8958_Score/Main.java
@@ -14,10 +14,19 @@ public class Main {
             String test = "";
             test = input.next();
             int[] score = new int[test.length()];
-
+            int sum = 0;
             for(int j = 0; j < test.length(); ++j) {
-                if(test[j].val == "O")
+                if(j == 0 && test.charAt(j) == 'O') {
+                    score[j] = 1;
+                } else if(test.charAt(j) == 'O') {
+                    score[j] = score[j-1] + 1;
+                } else {
+                    score[j] = 0;
+                }
+
+                sum += score[j];
             }
+            System.out.println(sum);
         }
     }
 }
