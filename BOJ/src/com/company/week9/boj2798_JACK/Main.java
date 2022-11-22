afdda05995e2bc8161f46596c21351c893db5f0b
hwlee0103 <sdiverHyeWon0103@gmail.com>
2022-11-22 20:33:36 +0900

    BOJ 2798 블랙잭

---
+++ BOJ/src/com/company/week9/boj2798_JACK/Main.java
@@ -0,0 +1,19 @@
+package com.company.week9.boj2798_JACK;
+
+import java.util.Scanner;
+
+/**
+ * BOJ 2798 블랙잭
+ */
+public class Main {
+    public static void main(String args[]) {
+        Scanner input = new Scanner(System.in);
+        int n = input.nextInt();
+        int m = input.nextInt();
+        int[] cards = new int[n];
+
+        //재귀 함수
+        //depth 3까지
+        //3개의 합 m 넘지 않는 최대 수 일 경우 ans 업데이트
+    }
+}
