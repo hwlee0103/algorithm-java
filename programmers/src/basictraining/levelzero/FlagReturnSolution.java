604bf417bbb3d297c5160c31a8b8d5bc6d9455b3
hwlee0103 <sdiverHyeWon0103@gmail.com>
2024-03-14 21:44:49 +0900

    Programmers-basic-lv0_flag에 따라 다른 값 반환하기_Solved

---
+++ programmers/src/basictraining/levelzero/FlagReturnSolution.java
@@ -0,0 +1,31 @@
+package basictraining.levelzero;
+
+import java.util.Scanner;
+
+/**
+ * Level : 0
+ * Title : Flag에 따라 다른 값 반환하기
+ */
+public class FlagReturnSolution {
+    public static int solution(int a, int b, boolean flag) {
+        int answer = 0;
+
+        if(flag == true) {
+            answer = a + b;
+        } else {
+            answer = a - b;
+        }
+        return answer;
+    }
+
+    public static void main(String args[]) {
+        Scanner input = new Scanner(System.in);
+        int n = input.nextInt();
+        int m = input.nextInt();
+        boolean flag = input.nextBoolean();
+
+        int answer = solution(n, m, flag);
+
+        System.out.println("Answer is : " + answer);
+    }
+}
