e2fc3970122f0b0046631f5c8a76340f01ec9602
hwlee0103 <sdiverHyeWon0103@gmail.com>
2024-03-19 21:40:27 +0900

    Programmers-basic-lv0_ 문자열 붙여서 출력하기_solved

---
+++ programmers/src/basictraining/levelzero/PrintAttachedString.java
@@ -0,0 +1,22 @@
+package basictraining.levelzero;
+
+import java.util.Scanner;
+
+/**
+ * Level : 0
+ * Title : 문자열 붙여서 출력하기
+ *
+ * Started : 2024-03-19
+ * Solved : 2024-03-19
+ *
+ */
+public class PrintAttachedString {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String a = sc.next();
+        String b = sc.next();
+
+        System.out.print(a);
+        System.out.println(b);
+    }
+}
