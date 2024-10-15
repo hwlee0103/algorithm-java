899882f2679ac147a87918da459d2c03787c70c4
hwlee0103 <sdiverHyeWon0103@gmail.com>
2024-10-15 12:34:54 +0900

    Modify TextsModify Texts

--- programmers/src/java_problemsolving/levelzero/introductionsofps/AddingFraction.java
+++ programmers/src/java_problemsolving/levelzero/introductionsofps/AddingFraction.java
@@ -9,17 +9,6 @@ import java.util.Scanner;
  * Started : 2024-04-22
  * Solved : 2024-04-22
  *
- * 문제
- * 첫 번째 분수의 분자와 분모를 뜻하는 numer1, denom1, 두 번째 분수의 분자와 분모를 뜻하는 numer2, denom2가 매개변수로 주어집니다.
- * 두 분수를 더한 값을 기약 분수로 나타냈을 때 분자와 분모를 순서대로 담은 배열을 return 하도록 solution 함수를 완성해보세요.
- *
- * 제한사항
- * 0 <numer1, denom1, numer2, denom2 < 1,000
- *
- * 입출력 예
- * numer1	denom1	numer2	denom2	result
- * 1	    2	    3	    4	    [5, 4]
- * 9	    2	    1	    3	    [29, 6]
  *
  */
 public class AddingFraction {
--- programmers/src/java_problemsolving/levelzero/introductionsofps/NumOfSeven.java
+++ programmers/src/java_problemsolving/levelzero/introductionsofps/NumOfSeven.java
@@ -10,18 +10,7 @@ import java.util.Scanner;
  * Started : 2024-05-26
  * Solved : 2024-05-26
  *
- * 문제
- * 머쓱이는 행운의 숫자 7을 가장 좋아합니다.
- * 정수 배열 array가 매개변수로 주어질 때, 7이 총 몇 개 있는지 return 하도록 solution 함수를 완성해보세요.
  *
- * 제한사항
- * 1 ≤ array의 길이 ≤ 100
- * 0 ≤ array의 원소 ≤ 100,000
- *
- * 입출력 예
- * array	    result
- * [7, 77, 17]	4
- * [10, 29]	    0
  *
  */
 public class NumOfSeven {
