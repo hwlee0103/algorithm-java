3f2d8e834a9557970564ecc6973e8b0bb3a76b60
hwlee0103 <sdiverHyeWon0103@gmail.com>
2024-10-18 15:27:48 +0900

    Modify TextsModify Texts

--- programmers/src/java_problemsolving/levelzero/introductionsofps/AddingBinaries.java
+++ programmers/src/java_problemsolving/levelzero/introductionsofps/AddingBinaries.java
@@ -10,21 +10,6 @@ import java.util.Scanner;
  * Started : 2024-06-19
  * Solved : 2024-06-19
  *
- * 문제
- * 이진수를 의미하는 두 개의 문자열 bin1과 bin2가 매개변수로 주어질 때, 두 이진수의 합을 return하도록 solution 함수를 완성해주세요.
- * 시켜먹은 치킨의 수 chicken이 매개변수로 주어질 때 받을 수 있는 최대 서비스 치킨의 수를 return하도록 solution 함수를 완성해주세요.
- *
- * 제한사항
- * return 값은 이진수를 의미하는 문자열입니다.
- * 1 ≤ bin1, bin2의 길이 ≤ 10
- * bin1과 bin2는 0과 1로만 이루어져 있습니다.
- * bin1과 bin2는 "0"을 제외하고 0으로 시작하지 않습니다.
- *
- * 입출력 예
- * bin1	    bin2	result
- * "10"	    "11"	"101"
- * "1001"	"1111"	"11000"
- * 100001   1010101  1110110
  *
  */
 public class AddingBinaries {
