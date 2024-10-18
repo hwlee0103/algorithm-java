57cdadd83c2ec7ee47d0162815386ec55f384c16
hwlee0103 <sdiverHyeWon0103@gmail.com>
2024-10-18 15:30:29 +0900

    Modify Texts

--- programmers/src/java_problemsolving/levelzero/introductionsofps/DontLikeEvenNum.java
+++ programmers/src/java_problemsolving/levelzero/introductionsofps/DontLikeEvenNum.java
@@ -10,16 +10,6 @@ import java.util.Scanner;
  * Started : 2024-04-23
  * Solved : 2024-04-23
  *
- * 문제
- * 정수 n이 매개변수로 주어질 때, n 이하의 홀수가 오름차순으로 담긴 배열을 return하도록 solution 함수를 완성해주세요.
- *
- * 제한사항
- * 1 ≤ n ≤ 100
- *
- * 입출력 예
- * n	result
- * 10	[1, 3, 5, 7, 9]
- * 15	[1, 3, 5, 7, 9, 11, 13, 15]
  *
  */
 public class DontLikeEvenNum {
