4c7970f273497c0d95a17bbdf9461484f63adfee
hwlee0103 <sdiverHyeWon0103@gmail.com>
2024-10-21 12:52:55 +0900

    Modify Texts

--- programmers/src/java_problemsolving/levelzero/introductionsofps/EvenOddNumber.java
+++ programmers/src/java_problemsolving/levelzero/introductionsofps/EvenOddNumber.java
@@ -10,17 +10,6 @@ import java.util.Scanner;
  * Started : 2024-04-25
  * Solved : 2024-04-25
  *
- * 문제
- * 정수가 담긴 리스트 num_list가 주어질 때, num_list의 원소 중 짝수와 홀수의 개수를 담은 배열을 return 하도록 solution 함수를 완성해보세요.
- *
- * 제한사항
- * 1 ≤ num_list의 길이 ≤ 100
- * 0 ≤ num_list의 원소 ≤ 1,000
- *
- * 입출력 예
- * num_list	        result
- * [1, 2, 3, 4, 5]	[2, 3]
- * [1, 3, 5, 7]	    [0, 4]
  *
  */
 public class EvenOddNumber {
