833bff67a643ac3cdc2ceccc4de90f76b5c3abcb
hwlee0103 <sdiverHyeWon0103@gmail.com>
2024-10-22 13:00:29 +0900

    Modify Texts

--- programmers/src/java_problemsolving/levelzero/introductionsofps/FindingCompositeNum.java
+++ programmers/src/java_problemsolving/levelzero/introductionsofps/FindingCompositeNum.java
@@ -9,17 +9,6 @@ import java.util.Scanner;
  * Started : 2024-05-06
  * Solved : 2024-05-06
  *
- * 문제
- * 약수의 개수가 세 개 이상인 수를 합성수라고 합니다.
- * 자연수 n이 매개변수로 주어질 때 n이하의 합성수의 개수를 return하도록 solution 함수를 완성해주세요.
- *
- * 제한사항
- * 1 ≤ n ≤ 100
- *
- * 입출력 예
- * n	result
- * 10	5
- * 15	8
  *
  */
 public class FindingCompositeNum {
