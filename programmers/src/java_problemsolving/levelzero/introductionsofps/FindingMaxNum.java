ea86e7debe8eccdc30146e30c811907d18f8d2cc
hwlee0103 <sdiverHyeWon0103@gmail.com>
2024-10-23 13:29:31 +0900

    Modify Texts

--- programmers/src/java_problemsolving/levelzero/introductionsofps/FindingMaxNum.java
+++ programmers/src/java_problemsolving/levelzero/introductionsofps/FindingMaxNum.java
@@ -10,18 +10,6 @@ import java.util.Scanner;
  * Started : 2024-05-17
  * Solved : 2024-05-17
  *
- * 문제
- * 정수 배열 array가 매개변수로 주어질 때, 가장 큰 수와 그 수의 인덱스를 담은 배열을 return 하도록 solution 함수를 완성해보세요.
- *
- * 제한사항
- * 1 ≤ array의 길이 ≤ 100
- * 0 ≤ array 원소 ≤ 1,000
- * array에 중복된 숫자는 없습니다.
- *
- * 입출력 예
- * array	        result
- * [1, 8, 3]	    [8, 1]
- * [9, 10, 11, 8]	[11, 2]
  *
  */
 public class FindingMaxNum {
