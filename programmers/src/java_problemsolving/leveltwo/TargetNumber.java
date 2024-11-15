8b7080b7724749ebcbef892622a082c1843afd4e
hwlee0103 <sdiverHyeWon0103@gmail.com>
2024-11-15 13:06:57 +0900

    Programmers_깊이/너비 우선 탐색(DFS/BFS) - lv2_타겟 넘버: solving

--- programmers/src/java_problemsolving/leveltwo/TargetNumber.java
+++ programmers/src/java_problemsolving/leveltwo/TargetNumber.java
@@ -16,15 +16,16 @@ import java.util.Scanner;
 public class TargetNumber {
     public int solution(int[] numbers, int target) {
         int answer = 0;
-        boolean[] check = new boolean[numbers.length];
+        int[] check = new int[numbers.length];
         answer = func(numbers, target, 0, 0, check);
 
         return answer;
     }
 
-    public int func(int[] numbers, int target, int nowIdx, int nowRes, boolean[] check) {
+    public int func(int[] numbers, int target, int nowIdx, int nowRes, int[] check) {
         int cnt = 0;
         if(nowIdx == numbers.length) {
+            System.out.println(Arrays.toString(check));
             if(nowRes == target) {
                 return 1;
             } else return 0;
@@ -32,13 +33,16 @@ public class TargetNumber {
             return 0;
         }
 
+        // TODO:
+        // 모든 원소에 대해 +와 -를 노드로 생각하고 순서대로 탐색해나가야 함
+        // + - 모두 이미 실행 했을 때 방문 횟수 2
         for(int i = 0; i < numbers.length; i++) {
-            if(check[i]) continue;
-            check[i] = true;
+            if(check[i] != 0) continue;
+            check[i] = 1;
             cnt += func(numbers, target, nowIdx + 1, nowRes + numbers[i], check);
-            check[i] = false;
+            check[i] = -1;
             cnt += func(numbers, target, nowIdx + 1, nowRes - numbers[i], check);
-//            check[i] = false;
+            check[i] = 2;
         }
         return cnt;
     }
