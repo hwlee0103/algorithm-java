b9e8d06abc7db8aee4d6abfa9cd01ef4d2871c30
hwlee0103 <sdiverHyeWon0103@gmail.com>
2024-11-21 12:33:02 +0900

    Programmers_해시 - lv2_전화번호 목록: solved

--- programmers/src/java_problemsolving/leveltwo/PhoneNumberList.java
+++ programmers/src/java_problemsolving/leveltwo/PhoneNumberList.java
@@ -1,5 +1,6 @@
 package java_problemsolving.leveltwo;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Scanner;
 
@@ -16,17 +17,25 @@ import java.util.Scanner;
 public class PhoneNumberList {
     public boolean solution(String[] phone_book) {
         boolean answer = true;
-        HashMap<String, Integer> map = new HashMap<>();
-        // TODO: 해결 방법 먼저 생각하기
-        for(int i = 0; i < phone_book.length; i++) {
-            map.put(phone_book[i], 1);
+        Arrays.sort(phone_book);
+//        System.out.println(Arrays.toString(phone_book));
+
+        String nowPre = phone_book[0];
+        for(int i = 1; i < phone_book.length;i++) {
+            if(phone_book[i].indexOf(nowPre) == 0) {
+               answer = false;
+               break;
+            } else {
+                nowPre = phone_book[i];
+            }
         }
+
         return answer;
     }
 
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        String[] phone_book = input.nextLine().replaceAll("\"", "").split(", ");
+        String[] phone_book = input.nextLine().replaceAll("\"", "").split(",");
         System.out.println(new PhoneNumberList().solution(phone_book));
     }
 }
