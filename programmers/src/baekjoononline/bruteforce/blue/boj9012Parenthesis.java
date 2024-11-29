d1c411ca4064e5c8607cecef3ad9133503a40964
hwlee0103 <sdiverHyeWon0103@gmail.com>
2024-11-29 12:34:46 +0900

    BOJ9012_Stack_Lv.Blue4_괄호:  solved

--- programmers/src/baekjoononline/bruteforce/blue/boj9012Parenthesis.java
+++ programmers/src/baekjoononline/bruteforce/blue/boj9012Parenthesis.java
@@ -1,15 +1,49 @@
 package baekjoononline.bruteforce.blue;
 
+import java.util.Scanner;
+import java.util.Stack;
+
 /**
  * Level : Blue 4
  * Title : 괄호 Parenthesis
- * 문제 유형 :
+ * 문제 유형 : 스택
  *
  * Started : 2024-11-26
- * Solved : 2024-
+ * Solved : 2024-11-29
  *
  *
  */
 public class boj9012Parenthesis {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int n = input.nextInt();
+        input.nextLine();
 
+        while(n > 0) {
+            String str = input.nextLine();
+            Stack<String> stack = new Stack<>();
+
+            for(int i = 0; i < str.length(); i++) {
+                if(str.charAt(i) == '(') {
+                    stack.push(str.charAt(i) + "");
+                } else {
+                    if(stack.isEmpty()) {
+                        stack.push(str.charAt(i) + "");
+                    } else {
+                        if(stack.peek().equals("(")) {
+                            stack.pop();
+                        } else {
+                            stack.push(str.charAt(i) + "");
+                        }
+                    }
+                }
+            }
+            n--;
+            if(!stack.isEmpty()) {
+                System.out.println("NO");
+            } else {
+                System.out.println("YES");
+            }
+        }
+    }
 }
