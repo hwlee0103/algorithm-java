6fdeb1b6d00d8dc6a9e692cc120e68979ba8f34a
hwlee0103 <sdiverHyeWon0103@gmail.com>
2025-01-13 21:33:55 +0900

    BOJ_1654_Binary Search_Lv.Blue2_랜선 자르기:  solved

--- BOJ/src/baekjoononline/binarysearch/blue/boj1654CuttingLan.java
+++ BOJ/src/baekjoononline/binarysearch/blue/boj1654CuttingLan.java
@@ -8,7 +8,7 @@ import java.util.Scanner;
  * 문제 유형 : 이분 탐색 Binary Search
  *
  * Started : 2025-01-10
- * Solved : 2025-01-
+ * Solved : 2025-01-13
  *
  *
  */
@@ -25,25 +25,23 @@ public class boj1654CuttingLan {
             maxLen = Math.max(maxLen, arr[i]);
         }
 
-        long low = 1, high = maxLen;
-        long answer = 0;
+        long low = 1, high = maxLen + 1; // upper bound
         long mid = low + (high - low) / 2;
         while(low < high) {
             // count
-            int cnt = 0;
+            long cnt = 0;
             for(int i = 0; i < arr.length; i++) {
                 cnt += arr[i]/mid;
             }
 
             if(cnt >= n) {
-                if(cnt == n) answer = Math.max(answer, mid);
                 low = mid + 1;
             } else {
-                high = mid - 1;
+                high = mid;
             }
 
             mid = low + (high - low) / 2;
         }
-        System.out.println(answer);
+        System.out.println(mid - 1);
     }
 }
