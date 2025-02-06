663373b9fc595b0d3b449fabc840909919e2040c
hwlee0103 <sdiverHyeWon0103@gmail.com>
2025-02-06 12:58:03 +0900

    BOJ_4948_Prime Numbers_Lv.Blue2_베르트랑 공준: solved

--- BOJ/src/baekjoononline/primenumbers/blue/boj4948BertrandPostulate.java
+++ BOJ/src/baekjoononline/primenumbers/blue/boj4948BertrandPostulate.java
@@ -4,7 +4,6 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.Scanner;
 
 /**
  * Level : Blue 2
@@ -12,7 +11,7 @@ import java.util.Scanner;
  * 문제 유형 : 소수 Prime Numbers, 에라토스테네스의 체 Sieve of Eratosthenes
  *
  * Started : 2025-02-05
- * Solved : 2025-02-
+ * Solved : 2025-02-06
  *
  *
  */
