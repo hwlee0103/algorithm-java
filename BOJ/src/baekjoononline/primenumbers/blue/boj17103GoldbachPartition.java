7120e784a9c9469e597664bef7692d2f60b42c74
hwlee0103 <sdiverHyeWon0103@gmail.com>
2025-02-20 12:31:23 +0900

    BOJ_17103_Prime Numbers_Lv.Blue2_골드바흐 파티션: solved

--- BOJ/src/baekjoononline/primenumbers/blue/boj17103GoldbachPartition.java
+++ BOJ/src/baekjoononline/primenumbers/blue/boj17103GoldbachPartition.java
@@ -1,5 +1,6 @@
 package baekjoononline.primenumbers.blue;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 /**
@@ -25,7 +26,31 @@ public class boj17103GoldbachPartition {
         }
 
         boolean[] isPrime = new boolean[maxNum + 1];
-        // TODO: 에라토스테네스의 체
+        Arrays.fill(isPrime, true);
+        isPrime[0] = isPrime[1] = false;
+        sieveOfEratosthenes(isPrime);
 
+        for(int query: queries) {
+
+        }
+    }
+
+    // 골드바흐 파티션
+    public static int goldbachPartition(int query, boolean[] isPrime) {
+        int answer = 0;
+        // TODO: 골드바흐 파티션 판별
+        
+        return answer;
+    }
+
+    // 소수판별 - 에라토스테네스의 체
+    public static void sieveOfEratosthenes(boolean[] isPrime) {
+        for(int i = 2; i * i <= isPrime.length; ++i) {
+            if(isPrime[i]) {
+                for(int j = i * i; j <= isPrime.length; ++j) {
+                    isPrime[j] = false;
+                }
+            }
+        }
     }
 }
