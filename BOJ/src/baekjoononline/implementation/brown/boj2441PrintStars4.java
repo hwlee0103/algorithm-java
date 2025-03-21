1afe8024197627576fa9e76bfa1625ee396f28e4
hwlee0103 <sdiverHyeWon0103@gmail.com>
2025-03-21 12:31:54 +0900

    BOJ_2441_Implementation_Lv.Brown3_별 찍기 - 4: solved

---
+++ BOJ/src/baekjoononline/implementation/brown/boj2441PrintStars4.java
@@ -0,0 +1,33 @@
+package baekjoononline.implementation.brown;
+
+import java.util.Scanner;
+
+/**
+ * Level : Brown 3
+ * Title : 2444 별 찍기 - 7
+ * 문제 유형 : 구현 Implementation
+ *
+ * Started : 2025-03-21
+ * Solved : 2025-03-
+ *
+ *
+ *
+ *
+ */
+
+public class boj2441PrintStars4 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for(int i = 0;i < n; ++i) {
+            int j = 0;
+            for(j = 0; j < i; ++j) {
+                System.out.print(" ");
+            }
+            for(int k = j; k < n; ++k) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
