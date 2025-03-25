6a08315d3eae9c28378b0bd376bae6923a14ee84
hwlee0103 <sdiverHyeWon0103@gmail.com>
2025-03-25 12:50:48 +0900

    BOJ_24444_Implementation_Lv.Brown3_별 찍기 - 7: solving

--- BOJ/src/baekjoononline/implementation/brown/boj2444PrintStars7.java
+++ BOJ/src/baekjoononline/implementation/brown/boj2444PrintStars7.java
@@ -7,7 +7,7 @@ import java.util.Scanner;
  * Title : 2444 별 찍기 - 7
  * 문제 유형 : 구현 Implementation
  *
- * Started : 2025-03-21
+ * Started : 2025-03-25
  * Solved : 2025-03-
  *
  *
@@ -19,8 +19,16 @@ public class boj2444PrintStars7 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         int n = input.nextInt();
-        for(int i = 0;i < 2*n-1; ++i) {
-            
+        for(int i = 1;i <= 2*n-1; ++i) {
+            for(int j = 1; j <= 2*n-1; ++j) {
+                int x = 2 * i - 1;
+                int y = ((2 * j - 1) - x) /2;
+                if(j <= y) {
+                    System.out.print(" ");
+                } else if(j <= x) {
+                    System.out.print("*");
+                } else System.out.println();
+            }
         }
     }
 }
