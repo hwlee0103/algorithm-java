86960dbfbbf18f9f79f62ba09ef72ee7613195d1
hwlee0103 <sdiverHyeWon0103@gmail.com>
2025-04-02 12:42:56 +0900

    BOJ_24416_Dynamic Programming_Lv.Brown1_알고리즘 수업 - 피보나치 수 1: solved

--- BOJ/src/baekjoononline/dynamicprogramming/brown/boj24416AlgorithmFibonacciNumber1.java
+++ BOJ/src/baekjoononline/dynamicprogramming/brown/boj24416AlgorithmFibonacciNumber1.java
@@ -9,26 +9,33 @@ import java.util.Scanner;
  * 문제 유형 : 동적계획법 Dynamic Programming
  *
  * Started : 2025-04-01
- * Solved : 2025-04-
+ * Solved : 2025-04-02
  *
  *
  */
 
 public class boj24416AlgorithmFibonacciNumber1 {
+    public static int reqCnt = 0;
+    public static int dpCnt = 0;
+
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         int n = input.nextInt();
         int[] answer = new int[2];
-        // TODO: 값이 아니라 실행 횟수 출력
+        // 값이 아니라 실행 횟수 출력
         answer[0] = recursiveFibo(n);
         answer[1] = dpFibo(n);
 
-        System.out.println(Arrays.toString(answer).replaceAll("\\[", "").replaceAll("]", ""));
+//        System.out.println(Arrays.toString(answer).replaceAll("\\[", "").replaceAll("]", ""));
+        System.out.println(reqCnt + " " + dpCnt);
     }
 
     public static int recursiveFibo(int n) {
-        if(n == 0 || n == 1) return n;
-        if(n == 2) return 1;
+        if(n == 0) return 0;
+        if(n == 1 || n == 2){
+            reqCnt++;
+            return 1;
+        }
         return recursiveFibo(n - 1) + recursiveFibo(n - 2);
     }
 
@@ -36,6 +43,7 @@ public class boj24416AlgorithmFibonacciNumber1 {
         int[] dp = new int[n + 1];
         dp[1] = dp[2] = 1;
         for(int i = 3; i <= n; i++) {
+            dpCnt++;
             dp[i] = dp[i - 1] + dp[i - 2];
         }
         return dp[n];
