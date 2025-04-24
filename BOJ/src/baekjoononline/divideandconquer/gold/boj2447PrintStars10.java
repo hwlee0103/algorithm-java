f91f7d8a40934560f2edd1648882d6ad1f1deeea
hwlee0103 <sdiverHyeWon0103@gmail.com>
2025-04-24 15:47:44 +0900

    BOJ_2447_DivideAndConquer_Lv.Gold5_별 찍기 - 10: solving

--- BOJ/src/baekjoononline/divideandconquer/gold/boj2447PrintStars10.java
+++ BOJ/src/baekjoononline/divideandconquer/gold/boj2447PrintStars10.java
@@ -1,5 +1,6 @@
 package baekjoononline.divideandconquer.gold;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 /**
@@ -17,8 +18,36 @@ public class boj2447PrintStars10 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         int N = input.nextInt();
+        char[][] starBoard = new char[N][N];
 
+        // TODO: BufferWriter 로 바꿔보기
 
+        makeStars(starBoard, 0, 0, N);
 
+        for(int i = 0; i < N; i++) {
+            for(int j = 0; j < N; j++) {
+                if(starBoard[i][j] == '\0') {
+                    System.out.print(" ");
+                }
+                else System.out.print(starBoard[i][j]);
+            }
+            System.out.println();
+        }
+    }
+
+    public static void makeStars(char[][] starBoard, int startX, int startY, int size) {
+        int[] dx = {0, 1, 2};
+        int[] dy = {0, 1, 2};
+
+        if(size == 1) {
+            starBoard[startX][startY] = '*';
+            return;
+        }
+        for(int i = 0; i < 3; ++i) {
+            for (int j = 0; j < 3; ++j) {
+                if (i == 1 && j == 1) continue;
+                makeStars(starBoard, startX + dx[i] * (size / 3), startY + dy[j] * (size / 3), size / 3);
+            }
+        }
     }
 }
