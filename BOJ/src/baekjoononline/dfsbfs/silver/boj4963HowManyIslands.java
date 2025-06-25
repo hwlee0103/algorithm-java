af1ae38040786618b8b809c9a23cfc5f8b679d62
hwlee0103 <sdiverHyeWon0103@gmail.com>
2025-06-25 12:44:46 +0900

    BOJ 4963 / DFSandBFS / Lv.Silver2 섬의 개수: solving

---
+++ BOJ/src/baekjoononline/dfsbfs/silver/boj4963HowManyIslands.java
@@ -0,0 +1,48 @@
+package baekjoononline.dfsbfs.silver;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+/**
+ * Level : Sliver 2
+ * Title : 섬의 개수 (DFS)
+ * 문제 유형 : DFS and BFS
+ *
+ * Started : 2025-06-25
+ * Solved : 2025-06-
+ *
+ *
+ */
+
+public class boj4963HowManyIslands {
+    public static void main(String[] args) {
+        
+//        try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
+//            String line;
+//            String firstline = "";
+//            // input
+//            while((line = br.readLine()) != null) {
+//                if(line.equals("0 0")) break;
+//                if(firstline.equals("")) {
+//                    firstline = line;
+//                } else {
+//                    int[] mapInfo = Arrays.stream(line.split(" ")).mapToInt(Integer::parseInt).toArray();
+//                    int[][] map = new int[mapInfo[0]][mapInfo[1]];
+//
+//
+//
+//                    firstline = "";
+//                }
+//            }
+//
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+    }
+
+    public static void dfs(int x, int y) {
+
+    }
+}
