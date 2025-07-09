6ce032d0cf6e98e7418a098f7b3510009eb6f8ad
hwlee0103 <sdiverHyeWon0103@gmail.com>
2025-07-09 12:53:10 +0900

    BOJ 2178 / DFSandBFS / Lv.Silver1 미로 탐색: solved

--- BOJ/src/baekjoononline/dfsbfs/silver/boj2178MazeNavigation.java
+++ BOJ/src/baekjoononline/dfsbfs/silver/boj2178MazeNavigation.java
@@ -1,8 +1,11 @@
 package baekjoononline.dfsbfs.silver;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.LinkedList;
 import java.util.Queue;
-import java.util.Scanner;
+import java.util.StringTokenizer;
 
 /**
  * Level : Sliver 1
@@ -16,52 +19,47 @@ import java.util.Scanner;
  */
 
 public class boj2178MazeNavigation {
-    public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        int N = input.nextInt();
-        int M = input.nextInt();
-        int[][] maze = new int[N + 1][M + 1];
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        for(int i = 1; i <= N; ++i) {
-            String str = input.next();
-            for(int j = 1; j <= M; ++j) {
-                maze[i][j] = str.charAt(j - 1) - '0';
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        int[][] maze = new int[N][M];
+        boolean[][] visited = new boolean[N][M];
+
+        for (int i = 0; i < N; i++) {
+            String line = br.readLine();
+            for (int j = 0; j < M; j++) {
+                maze[i][j] = line.charAt(j) - '0';
             }
         }
 
-        boolean[][] visited = new boolean[N + 1][M + 1];
-        int answer = bfs(1, 1, visited, maze);
+        int answer = bfs(0, 0, visited, maze);
 
         System.out.println(answer);
     }
 
-    public static class Pos {
-        int x;
-        int y;
-
-        public Pos(int x, int y) {
-            this.x = x;
-            this.y = y;
-        }
-    }
-
     public static int bfs(int x, int y, boolean[][] visited, int[][] maze) {
-        Queue<Pos> queue = new LinkedList<>();
-        queue.add(new Pos(x, y));
+        Queue<int[]> queue = new LinkedList<>();
+        queue.add(new int[]{x, y});
 
         int[] dx = {-1, 0, 1, 0};
         int[] dy = {0, -1, 0, 1};
 
         while(!queue.isEmpty()) {
-            Pos pos = queue.poll();
-            visited[pos.x][pos.y] = true;
+            int[] pos = queue.poll();
+            visited[pos[0]][pos[1]] = true;
 
             for(int d = 0; d < 4; ++d) {
-                int nx = pos.x + dx[d];
-                int ny = pos.y + dy[d];
+                int nx = pos[0] + dx[d];
+                int ny = pos[1] + dy[d];
 
-                if(nx <= 0 || ny <= 0 || nx > maze.length || ny > maze[0].length || visited[nx][ny] || maze[nx][ny] == 0) { continue; }
-                queue.add(new Pos(nx, ny));
+                if(nx < 0 || ny < 0 || nx >= maze.length || ny >= maze[0].length || visited[nx][ny] || maze[nx][ny] == 0) { continue; }
+                queue.add(new int[]{nx, ny});
+                visited[nx][ny] = true;
+                maze[nx][ny] = maze[pos[0]][pos[1]] + 1;
             }
         }
 
