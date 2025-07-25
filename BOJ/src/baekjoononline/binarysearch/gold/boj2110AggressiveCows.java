7cc786bb278acdb9fd8f170b822d10608c2f5642
hwlee0103 <sdiverHyeWon0103@gmail.com>
2025-07-25 12:58:31 +0900

    BOJ 2110 / Binary Search - Parametric Search / Lv.Gold 4 공유기 설치: solving

--- BOJ/src/baekjoononline/binarysearch/gold/boj2110AggressiveCows.java
+++ BOJ/src/baekjoononline/binarysearch/gold/boj2110AggressiveCows.java
@@ -29,10 +29,31 @@ public class boj2110AggressiveCows {
         int left = 1;
         int right = arr[N - 1] - arr[0];
 
+        // 그럼 현재 거리를 기준으로, arr에 몇 개의 공유기를 설치할 수 있느냐가 관건
+        // 만약 설치 갯수가 부족하다면 현재 거리를 줄이고
+        // 설치 갯수가 남는다면 현재 거리를 늘려야 -> 최대 거리
         while(left <= right) {
             int mid = (left + right) / 2;
-            
+            // todo: arr에서 설치 갯수 탐색
+            // todo: 이중 for문으로 시작점 변경해가면서 탐색하지 않아도 될지?
+            int cnt = 0;
+            if(cnt < C) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
         }
 
+    }
+
+    // dist: 인접한 공유기 사이의 거리 !
+    public static int checkArr(int[] arr, int dist) {
+        int res = 0;
+
+        for(int i=0; i<arr.length; i++) {
+
+        }
+
+        return res;
     }
 }
