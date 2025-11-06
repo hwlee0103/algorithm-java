d9b101856751afc05b34dbb0504d92213c1ace27
hwlee0103 <sdiverHyeWon0103@gmail.com>
2025-11-06 13:04:45 +0900

    LeetCode Lv.Easy / Array, Math / 66. Plus One - solved

--- LeetCode/src/Easy/PlusOne.java
+++ LeetCode/src/Easy/PlusOne.java
@@ -8,13 +8,26 @@ package Easy;
  * Started : 2025-11-06
  * Solved : 2025-11-06
  * Time: 06 min 48 sec
- * Algorithm: Array
+ * Algorithm: Array 사용 -> Math 사용
  *
  *
  *
  */
 public class PlusOne {
 
+    public int[] plusOne2(int[] digits) {
+        for(int i = digits.length - 1; i >= 0; --i) {
+            if(digits[i] < 9) { // 9 이하면 1증가하고 Return -> 가장 마지막 수만 + 1이므로 올림을 해도 다른 자리수도 여기서 처리 됨
+                digits[i]++;
+                return digits;
+            }
+            digits[i] = 0;
+        }
+        // 마지막 올림 -> 모두 9였을 경우만 해당됨
+        int[] res = new int[digits.length + 1];
+        res[0] = 1;
+        return res;
+    }
 
     public int[] plusOne(int[] digits) {
         int up = 0;
