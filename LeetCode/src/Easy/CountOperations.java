37a85a66bde18b1c3e49dc957cc3d8e87d5c740c
hwlee0103 <sdiverHyeWon0103@gmail.com>
2025-11-09 12:36:00 +0900

    LeetCode Lv.Easy / Math, Simulation, Weekly Contest 280 / 2169. Count Operations to Obtain Zero - solved

---
+++ LeetCode/src/Easy/CountOperations.java
@@ -0,0 +1,26 @@
+package Easy;
+
+/**
+ * Level : Easy
+ * Title : 2169. Count Operations to Obtain Zero
+ * Topic : Math, Simulation, Weekly Contest 280
+ *
+ * Started : 2025-11-08
+ * Solved : 2025-11-08
+ * Time: 02 min 53 sec
+ * Algorithm: Math, Simulation 사용
+ *
+ *
+ *
+ */
+public class CountOperations {
+    public int countOperations(int num1, int num2) {
+        int cnt = 0;
+        while(!(num1 == 0 || num2 == 0)) {
+            if(num1 >= num2) num1 -= num2;
+            else num2 -= num1;
+            cnt++;
+        }
+        return cnt;
+    }
+}
