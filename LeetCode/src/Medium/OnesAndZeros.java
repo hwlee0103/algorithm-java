3bfaf01db9fac9cb7e2c09099a9c5959d06f99dc
hwlee0103 <sdiverHyeWon0103@gmail.com>
2025-11-11 14:31:48 +0900

    LeetCode Lv.Medium / Array, String, Dynamic Programming / 474. Ones and Zeros - solved

--- LeetCode/src/Medium/OnesAndZeros.java
+++ LeetCode/src/Medium/OnesAndZeros.java
@@ -8,28 +8,32 @@ import java.util.Arrays;
  * Topic : Array, String, Dynamic Programming
  *
  * Started : 2025-11-11
- * Solved : 2025-11-
- * Time: 30 min 27 sec
- * Algorithm: 2차원 Knapsack 문제; Top-down으로 추정
- *
+ * Solved : 2025-11-11
+ * Time: 30 min 27 sec --
+ * Algorithm: 2차원 0/1 Knapsack 문제; Top-down으로 추정
+ * RE
  *
  *
  */
 
 public class OnesAndZeros {
     public int findMaxForm(String[] strs, int m, int n) {
-        // m = 5, n = 3
-        // [10, 0001, 111001, 1, 0]
-        // DP - 포함/ 불포함
-        int[][] memo = new int[m][n];
-        for(int i = 0; i < m; i++) Arrays.fill(memo[i], 0);
-        int res = dp(memo, m, n);
+        int[][] dp = new int[m+1][n+1];
 
+        for(String s : strs) {
+            int z = 0, o = 0;
+            for(char c: s.toCharArray()) {
+                if(c == '0') z++;
+                else o++;
+            }
+            // 0/1 knapsack
+            for(int i = m; i >= z; i--) {
+                for(int j = n; j >= o; j--) {
+                    dp[i][j] = Math.max(dp[i][j], dp[i-z][j-o] + 1);
+                }
+            }
+        }
 
-        return res;
-    }
-
-    public int dp(int[][] memo, int m, int n) {
-
+        return dp[m][n];
     }
 }
