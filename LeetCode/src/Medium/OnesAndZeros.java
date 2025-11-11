2aca43b90d31663054ce96f5e5b41da5f19d14fa
hwlee0103 <sdiverHyeWon0103@gmail.com>
2025-11-11 12:56:37 +0900

    LeetCode Lv.Medium / Array, String, Dynamic Programming / 474. Ones and Zeros - solving

--- LeetCode/src/Medium/OnesAndZeros.java
+++ LeetCode/src/Medium/OnesAndZeros.java
@@ -15,4 +15,17 @@ package Medium;
  */
 
 public class OnesAndZeros {
+    public int findMaxForm(String[] strs, int m, int n) {
+        // m = 5, n = 3
+        // [10, 0001, 111001, 1, 0]
+        // DP - 포함/ 불포함
+        int[][] memo = new int[m][n];
+        int res = dp(memo, m, n);
+
+        return res;
+    }
+
+    public int dp(int[][] memo, int m, int n) {
+
+    }
 }
