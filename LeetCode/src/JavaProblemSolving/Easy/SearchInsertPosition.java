c26f425751b7f17ee69143da5cf8d0529381a057
hwlee0103 <sdiverHyeWon0103@gmail.com>
2025-12-16 12:30:20 +0900

    LeetCode Lv.Easy / Array, Binary Search / 35. Search Insert Position - solving

--- LeetCode/src/JavaProblemSolving/Easy/SearchInsertPosition.java
+++ LeetCode/src/JavaProblemSolving/Easy/SearchInsertPosition.java
@@ -1,5 +1,12 @@
 package JavaProblemSolving.Easy;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * Level : Easy
  * Title : 35. Search Insert Position
@@ -15,14 +22,43 @@ package JavaProblemSolving.Easy;
  */
 
 public class SearchInsertPosition {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        String currentLevel = "JavaProblemSolving/Easy";
+        String currentClass = "SearchInsertPosition";
+        Path inputPath = Paths.get("LeetCode/src/" + currentLevel +"/input/"+ currentClass + "_input.txt");
+        Path outputPath = Paths.get("LeetCode/src/" + currentLevel +"/output/"+ currentClass + "_output.txt");
 
+        List<String> inputLines = Files.readAllLines(inputPath);
+        List<String> outputLines = Files.readAllLines(outputPath);
+
+        for(int i = 0; i < inputLines.size(); i++) {
+            System.out.println("Query #" + (i + 1));
+            String[] inputs = inputLines.get(i).split(" ");
+            String[] arr = inputs[0].replaceAll("\\[", "").replaceAll("]", "").split(",");
+            int target = Integer.parseInt(inputs[1]);
+
+            System.out.println("nums: " + Arrays.toString(arr));
+            System.out.println("target: " + target);
+            System.out.println("--------------------------");
+            int answer = searchInsert(Arrays.stream(arr).mapToInt(Integer::parseInt).toArray(), target);
+            System.out.println("answer: " + answer);
+
+            int output = Integer.valueOf(outputLines.get(i));
+            System.out.print(" ==> ");
+            if(answer == output) {
+                System.out.println("Success!");
+            } else {
+                System.out.println("Failed!");
+            }
+            System.out.println("=========================");
+
+        }
     }
 
     public static int searchInsert(int[] nums, int target) {
         int left = 0;
         int right = nums.length - 1;
-        int mid = (left + right) / 2;
+        int mid;
 
         while(left < right) {
             mid = (left + right) / 2;
