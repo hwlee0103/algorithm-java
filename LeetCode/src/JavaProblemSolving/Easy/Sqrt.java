02be73f97b3083f80bc22cbe815616c027f1b42a
hwlee0103 <sdiverHyeWon0103@gmail.com>
2026-01-08 12:27:05 +0900

    LeetCode Lv.Easy / Math, Binary Search / 69. Sqrt - solved

--- LeetCode/src/JavaProblemSolving/Easy/Sqrt.java
+++ LeetCode/src/JavaProblemSolving/Easy/Sqrt.java
@@ -1,5 +1,11 @@
 package JavaProblemSolving.Easy;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
 /**
  * Level : Easy
  * Title : 69. Sqrt
@@ -14,10 +20,32 @@ package JavaProblemSolving.Easy;
  *
  */
 public class Sqrt {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        String currentLevel = "JavaProblemSolving/Easy";
+        String currentClass = "Sqrt";
+        Path inputPath = Paths.get("LeetCode/src/" + currentLevel +"/input/"+ currentClass + "_input.txt");
+        Path outputPath = Paths.get("LeetCode/src/" + currentLevel +"/output/"+ currentClass + "_output.txt");
 
+        List<String> inputLines = Files.readAllLines(inputPath);
+        List<String> outputLines = Files.readAllLines(outputPath);
+
+        for(int i = 0; i < inputLines.size(); i++) {
+            System.out.println("Query #" + String.valueOf(i + 1));
+            int query = Integer.valueOf(inputLines.get(i));
+            System.out.println("n = " + query);
+            int answer = mySqrt(query);
+            int output = Integer.valueOf(outputLines.get(i));
+            System.out.println("-----------------");
+            System.out.println("Answer: " + answer);
+            System.out.print(" ==> ");
+            if(answer == output){
+                System.out.println("Success!");
+            } else System.out.println("Failed!");
+            System.out.println("=======================");
+        }
     }
-    public int mySqrt(int x) {
+
+    public static int mySqrt(int x) {
         return (int) Math.sqrt(x);
     }
 }
