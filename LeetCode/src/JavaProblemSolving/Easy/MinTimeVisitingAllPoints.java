45b815d4dd0a9696366ff3fa4db776fb9a3a117f
hwlee0103 <sdiverHyeWon0103@gmail.com>
2026-01-19 12:28:21 +0900

    LeetCode Lv.Easy / Array, Math, Geometry, Weekly Contest 164 / 1266. Minimum Time Visiting All Points - solved

--- LeetCode/src/JavaProblemSolving/Easy/MinTimeVisitingAllPoints.java
+++ LeetCode/src/JavaProblemSolving/Easy/MinTimeVisitingAllPoints.java
@@ -18,6 +18,22 @@ import java.util.List;
  * Algorithm: Math, Geometry
  *
  *
+ // 1. 시작 점 - 목적 점(다음 점) : x, y중 한 개라도 같으면 다른 좌표만 움직이면 됨 -> 그 만큼 count
+ // 2. 같은 게 없다면, 같은 게 생길 때 까지 대각선 움직임 (x, y 모두 변동) _+ 같아지면 다른거의 차이만큼 + count
+ // [[1,1],[3,4],[-1,0]]
+ // (1, 1) & [3, 4] -> 같은 게 없음
+ //                  -> 3 -1, 4 -1 중에 2가 더 작으므로 2만큼 대각선 이동 -> [1, 1] -> [3, 3]
+ //                  -> [3, 3] && [3, 4] -> 다른거 1칸
+ // ans += 3; (대각선 2, 수직 1)
+ // [3, 4] -> [-1, 0]
+ //                  -> -1 -3, -4 -> 둘 다 -4 -> 대각선 4칸 -> ans += 4;
+ // ans == 7;
+
+
+ // move horizontally
+ // move vertically
+ // move diagonally
+ *
  *
  */
 public class MinTimeVisitingAllPoints {
@@ -64,29 +80,10 @@ public class MinTimeVisitingAllPoints {
         int[] prev = points[0];
         for(int i = 1 ; i < points.length; i++) {
             int[] next = points[i];
-            if(prev[0] == next[0] || prev[1] == next[1]) {
-                count += Math.max(Math.abs(prev[0] - next[0]), Math.abs(prev[1] - next[1]));
-            } else {
-                count += Math.min(Math.abs(prev[0] - next[0]), Math.abs(prev[1] - next[1])) + Math.abs(next[1] - next[0]);
-            }
+            count += Math.max(Math.abs(prev[0] - next[0]), Math.abs(prev[1] - next[1]));
             prev = next;
         }
 
-        // 1. 시작 점 - 목적 점(다음 점) : x, y중 한 개라도 같으면 다른 좌표만 움직이면 됨 -> 그 만큼 count
-        // 2. 같은 게 없다면, 같은 게 생길 때 까지 대각선 움직임 (x, y 모두 변동) _+ 같아지면 다른거의 차이만큼 + count
-        // [[1,1],[3,4],[-1,0]]
-        // (1, 1) & [3, 4] -> 같은 게 없음
-        //                  -> 3 -1, 4 -1 중에 2가 더 작으므로 2만큼 대각선 이동 -> [1, 1] -> [3, 3]
-        //                  -> [3, 3] && [3, 4] -> 다른거 1칸
-        // ans += 3; (대각선 2, 수직 1)
-        // [3, 4] -> [-1, 0]
-        //                  -> -1 -3, -4 -> 둘 다 -4 -> 대각선 4칸 -> ans += 4;
-        // ans == 7;
-
-
-        // move horizontally
-        // move vertically
-        // move diagonally
         return count;
     }
 }
