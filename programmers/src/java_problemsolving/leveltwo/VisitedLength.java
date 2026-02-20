618aff5f21f65e27bbc5c938804528b2c4d30988
hwlee0103 <sdiverHyeWon0103@gmail.com>
2026-02-20 12:33:41 +0900

    Programmers Lv.2 / Summer/Winter Coding(~2018) / 49994 방문 길이 - solving

--- programmers/src/java_problemsolving/leveltwo/VisitedLength.java
+++ programmers/src/java_problemsolving/leveltwo/VisitedLength.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -16,6 +17,7 @@ import java.util.List;
  * Time: -
  * Algorithm: Simulation?
  * 제한: dirs의 길이는 500이하의 자연수
+ * bfs, 범위: +-5, 단 방문 시 포인트 방문이 아니라 '경로'가 방문되었는지 여부가 중요
  *
  */
 public class VisitedLength {
@@ -30,12 +32,33 @@ public class VisitedLength {
         List<String> outputLines = Files.readAllLines(outputPath);
 
         for(int i = 0; i < inputLines.size(); i++){
+            System.out.println("Query #" + String.valueOf(i + 1));
+            String dirs = inputLines.get(i).replaceAll("\"", "");
+            System.out.println("dirs: " + dirs);
+            int answer = solution(dirs);
 
+            System.out.println("---------------");
+            System.out.println("Answer: " + answer);
+            System.out.print(" ==> ");
+            if(answer == Integer.parseInt(outputLines.get(i))){
+                System.out.println("Success!");
+            } else System.out.println("Failed!");
+            System.out.println("=====================");
         }
     }
 
+    static class Position{
+        int x = 0;
+        int y = 0;
+        public Position(int x, int y){
+            this.x = x;
+            this.y = y;
+        }
+    }
     public static int solution(String dirs) {
         int answer = 0;
+        Position now = new Position(0, 0);
+
         return answer;
     }
 }
