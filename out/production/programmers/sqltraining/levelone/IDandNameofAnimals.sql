f06e5eee3c82311aa65a5de93b1d4179be8ac515
hwlee0103 <sdiverHyeWon0103@gmail.com>
2026-02-20 12:33:51 +0900

    Programmers Lv.1 / MySQL / 동물의 아이디와 이름 - solved

---
+++ out/production/programmers/sqltraining/levelone/IDandNameofAnimals.sql
@@ -0,0 +1,15 @@
+/**
+ * Level : 1
+ * Title : 동물의 아이디와 이름
+ * 문제 유형 : MYSQL
+ *
+ * Started : 2026-02-13
+ * Solved : 2026-02-13
+ *
+ *
+ */
+SELECT
+    ANIMAL_ID
+     , NAME
+FROM ANIMAL_INS
+ORDER BY ANIMAL_ID ASC
